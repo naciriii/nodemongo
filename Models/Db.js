794c74301395f5aeb3e3dd3748a83486e044ed14
Nacer Nsiri <nnsiri@unionkeynet.com>
2019-05-29 14:12:35 +0100

    administrator add

--- Models/Db.js
+++ Models/Db.js
@@ -7,6 +7,6 @@ let db = mongoose.connection
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  // we're connected!
-  console.log('connected to db ')
+
+
 });
--- routes/admin.js
+++ routes/admin.js
@@ -1,29 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const _usercontroller = require('../controller/UserController')();
-router.use((req, res, next) => {
-
-
-   
-    if(req.session.user === undefined) {
-  
-
-       
-        return res.redirect('/login');
-
-    }
-
-    return next();
-})
 
 router.get('/', _usercontroller.index);
 router.post('/', _usercontroller.store);
 router.patch('/', _usercontroller.update);
 router.get('/delete/:id', _usercontroller.remove);
 router.get('/:id', _usercontroller.show);
-
-
-
-
 
 module.exports = router;
