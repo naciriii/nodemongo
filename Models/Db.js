403122fa8ea5344db4c5aa39b94c4def390eb285
Nacer Nsiri <nnsiri@unionkeynet.com>
2019-05-29 13:52:49 +0100

    check testing database

--- Models/Db.js
+++ Models/Db.js
@@ -1,5 +1,5 @@
 const mongoose  = require('mongoose');
-let url = process.env.MONGODB_URI || "mongodb://nacer:moslem1990@cluster0-shard-00-00-tksix.mongodb.net:27017,cluster0-shard-00-01-tksix.mongodb.net:27017,cluster0-shard-00-02-tksix.mongodb.net:27017/Learning?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"; 
+let url = "mongodb://nacer:moslem1990@cluster0-shard-00-00-tksix.mongodb.net:27017,cluster0-shard-00-01-tksix.mongodb.net:27017,cluster0-shard-00-02-tksix.mongodb.net:27017/Learning?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"; 
 
 
 mongoose.connect(url, { useNewUrlParser: true });
